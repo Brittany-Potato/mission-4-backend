fc5cdb2dd15a92ac904161738a4fadf2af74ee7a
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-06-06 16:29:57 +1200

    IT WORKS

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
-//*~~~~~ Imports, constants and installs. ~~~~~
+//*~~~~~ Imports ~~~~~
 
-const express = require("express");
+require("dotenv").config();
+const { GoogleGenAI } = require('@google/genai');
+const axios = require('axios');
+const express = require('express')
 const app = express();
-const port = " ";
-import { GoogleGenAI } from "google/genai";
-const ai = new GoogleGenAI({ apikey: API_KEY })
 
-//*~~~~~ Middleware to parse JSON requests. ~~~~~
+//~~~~~
+
+const port = 3000;
+
+//*~~~~~ Variables ~~~~~
+
+const API_KEY = process.env.API_KEY;
+const ai = new GoogleGenAI({ apiKey: API_KEY });
+
+//*~~~~~ Middleware ~~~~~
 
 app.use(express.json());
 
-//*~~~~~ Endpoints ~~~~~
+//*~~~~ POST Endpoint ~~~~~
 
-//* Simple GET endpoint.
-app.get("./hello", (req, res) => {
-  res.json({ message: "Hello from Node.js!" });
-});
-
-//* Simple POST end point.
 app.post("./echo", (req, res) => {
-  res.json({
-    message: "You sent this",
-    data: req.body,
-  });
+    res.json({
+        message: "You sent this",
+        data: req.body,
+    });
 });
 
+//*~~~~~ Initial AI Function ~~~~~
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+}
+
+main();
 
 
 
 
+//*~~~~~ Start the server ~~~~~
 
-//*~~~~~ Start the server. ~~~~~
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`);
 });
