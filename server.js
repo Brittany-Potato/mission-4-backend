454344b8bd595b86b7d8fab4a8fbef8203077171
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-06-06 17:45:45 +1200

    First prompt draft added

--- server.js
+++ server.js
@@ -29,11 +29,19 @@ app.post("/echo", async (req, res) => {
     const response = await ai.models.generateContent({
         model: "gemini-2.0-flash",
         contents: "Explain how AI works in a few words",
+        config: {
+            systemInstruction: [
+                {
+                    role: "assistant",
+                    text: 'Your name is Tina. You are an insurance consultant for Turners Insurance. You are formal, helpful and supportive. When the conversation starts you will ask the customer between 5-8 questions to figure out what insurance plan the customer should go for. Your questions and responses will be dynamic allowing you to mix mostly formal conversation with partially informal humanoid responses.  Your first response will be “I’m Tina.  I help you to choose right insurance policy.  May I ask you a few personal questions to make sure I recommend the best policy for you?” and you will only respond if the user agrees to be asked. Your task is to asked questions to uncover what insurance plan the customer should go for. The insurance options include: Mechanical Breakdown Insurance (MBI), Comprehensive Car Insurance, Third Party Car Insurance. Example question: “do you need coverage for your own car or just 3rd party?”. At the end of your 5-8 questions you will recommend what insurance plan the customer should go for and why you recommend it, limiting your response to under 20 words.'
+                }
+            ]
+        }
     });
     console.log(response.text);
 
     res.json({
-        message: "You sent this",
+        message: response.text,
         data: req.body,
     });
 });
